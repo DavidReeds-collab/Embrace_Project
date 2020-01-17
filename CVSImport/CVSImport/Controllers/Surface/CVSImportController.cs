6529f497378c14f93a63bc095a19067ae8a5d121
martijnWouterse <martijn.wouterse@gmail.com>
2020-01-17 16:09:24 +0100

    Added initial project to the repository.

---
+++ CVSImport/CVSImport/Controllers/Surface/CVSImportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace CVSImport.Controllers.Surface
+{
+    public class CVSImportController : SurfaceController
+    {
+        public ActionResult ImportCSV()
+        {
+            return Content("Done");
+        }
+    }
+}
